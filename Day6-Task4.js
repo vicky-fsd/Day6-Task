c729da70939fe4138691095579cf089afc75a912
Vignesh D <137488863+vicky-fsd@users.noreply.github.com>
2023-07-04 21:40:59 +0530

    Create Day6-Task4.js

---
+++ Day6-Task4.js
@@ -0,0 +1,20 @@
+class UberPriceCalculator {
+  constructor(baseFare, costPerKm, costPerMinute) {
+    this.baseFare = baseFare;
+    this.costPerKm = costPerKm;
+    this.costPerMinute = costPerMinute;
+  }
+
+  calculatePrice(distanceInMiles, durationInMinutes) {
+    var distanceCost = this.costPerKm * distanceInMiles;
+    var durationCost = this.costPerMinute * durationInMinutes;
+    var totalPrice = this.baseFare + distanceCost + durationCost;
+    return totalPrice;
+  }
+}
+var calculator = new UberPriceCalculator(100, 2, 1);
+var distance = 10; // 5 miles
+var duration = 25; // 15 minutes
+
+var totalPrice = calculator.calculatePrice(distance, duration);
+console.log(`The total price of the Uber ride is Rs. ${totalPrice}`); //output : The total price of the Uber ride is Rs. 145
